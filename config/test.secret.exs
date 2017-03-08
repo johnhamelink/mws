725eaaed1f51831da8130639b92c61000362549d
John Hamelink <john@johnhamelink.com>
2017-03-08 03:21:57 +0000

    Remove test.secret.exs file

--- config/test.secret.exs
+++
@@ -1,9 +0,0 @@
-use Mix.Config
-
-config :mws,
-  endpoint: Mws.Config.endpoint(:europe),
-  seller_id: "AAAAAAAAAAAAAA",
-  marketplace_id: "AAAAAAAAAAAAAA",
-  access_key_id: "AAAAAAAAAAAAAA",
-  secret_key: "AAAAAAAAAAAAAA",
-  mws_auth_token: "1111-1111-1111"
