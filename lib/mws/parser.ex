edf07d3da076005eac15ccedb4ec35fd67270654
John Hamelink <john@johnhamelink.com>
2017-03-27 02:34:05 +0100

    Expose the headers in the response & turn the headers in the response into a map

--- lib/mws/parser.ex
+++ lib/mws/parser.ex
@@ -4,14 +4,24 @@ defmodule Mws.Parser do
   """
 
   def handle_response({:ok, %{body: body, headers: headers, status_code: _code}}) do
-    case get_content_type(headers) do
-      "text/xml"                        -> parse_xml(body)
-      "text/xml;charset=" <> _charset   -> parse_xml(body)
-      "text/plain;charset=" <> _charset ->
-        body
-        |> String.split("\r\n")
-        |> CSV.decode(seperator: ?\t, headers: true)
-    end
+    body =
+      case get_content_type(headers) do
+        "text/xml"                        -> parse_xml(body)
+        "text/xml;charset=" <> _charset   -> parse_xml(body)
+        "text/plain;charset=" <> _charset ->
+          body
+          |> String.split("\r\n")
+          |> CSV.decode(seperator: ?\t, headers: true)
+      end
+
+    # Turn the headers into a map
+    headers =
+      headers
+      |> Enum.reduce(%{}, fn({k, v}, acc) ->
+        Map.put(acc, k, v)
+      end)
+
+    %{headers: headers, body: body}
   end
 
   defp parse_xml(xml) do
